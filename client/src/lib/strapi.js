27d1f42c83679b840dc7b05d3e718a813d33c616
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2023-12-11 19:31:27 +0100

    index about created

---
+++ client/src/lib/strapi.js
@@ -0,0 +1,33 @@
+export default async function fetchApi({
+  endpoint,
+  query = undefined,
+  wrappedByKey = undefined,
+  wrappedByList = undefined,
+}) {
+  if (endpoint.startsWith("/")) {
+    endpoint = endpoint.slice(1);
+  }
+
+  const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
+
+  if (query) {
+    Object.entries(query).forEach(([key, value]) => {
+      url.searchParams.append(key, value);
+    });
+  }
+
+  console.log("Fetching...", url.toString());
+
+  const res = await fetch(url.toString());
+  let data = await res.json();
+
+  if (wrappedByKey) {
+    data = data[wrappedByKey];
+  }
+
+  if (wrappedByList) {
+    data = data[0];
+  }
+
+  return data;
+}
