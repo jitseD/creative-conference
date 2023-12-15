0f8f8b0ff892fb88c07be7a20d58d66e8db6d653
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2023-12-15 08:12:05 +0100

    base url added

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://jitsedekeyser.be",
+    base: "/creative-conference",
+    image: {
+        domains: ["localhost"],
+    },
+});
