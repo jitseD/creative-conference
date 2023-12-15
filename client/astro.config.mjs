9f73deb824d65f8b2f65c96636494a2dcc5df14b
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2023-12-15 16:23:56 +0100

    schedule structure finished

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://jitsedekeyser.be",
-    base: "/creative-conference",
+    base: "/web3/creative-conference",
     image: {
         domains: ["localhost"],
     },
