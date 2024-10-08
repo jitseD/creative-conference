495d31d782676d506f786806eb65e589a3b9be11
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-10-08 14:48:14 +0200

    website url changed

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://jitsedekeyser.be",
-    base: "/web3/creative-conference",
+    base: "/devine/creative-conference",
     image: {
         domains: ["localhost"],
     },
